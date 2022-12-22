60702998df680947085f49b5e13085919d766c7f
roman <r.stoian@adtelligent.com>
2022-12-22 20:50:58 +0200

    done hw02

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,76 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const Joi = require('joi');
+const contacts = require('./contacts.json');
 
-const listContacts = async () => {}
+const path = require("path");
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const schema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
 
-const removeContact = async (contactId) => {}
+const save = async () => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+};
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  return contacts;
+}
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  return contacts.find(item => item.id === contactId);
+}
+
+const removeContact = async (contactId) => {
+  const index = contacts.findIndex(item => item.id === contactId);
+
+  if (index >= 0) {
+    contacts.slice(index);
+    await save();
+
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const addContact = async (body) => {
+  const {error} = schema.validate(body);
+
+  if (error) {
+    return null;
+  } else {
+    const lastId = contacts[contacts.length - 1].id || 0;
+
+    body.id = lastId + 1;
+    contacts.push(body);
+    await save();
+
+    return body;
+  }
+}
+
+const updateContact = async (contactId, body) => {
+  const index = contacts.findIndex(item => item.id === contactId);
+
+  if (index >= 0) {
+    const updatedData = {...contacts[index], body};
+    const {error} = schema.validate(updatedData);
+
+    if (error) {
+      return {error: {message: 'missing fields', code: 400}}
+    }
+
+    contacts[index] = updatedData;
+    await save();
+
+    return {contact: body};
+  } else {
+    return {error: {message: 'Not found', code: 404}};
+  }
+}
 
 module.exports = {
   listContacts,
